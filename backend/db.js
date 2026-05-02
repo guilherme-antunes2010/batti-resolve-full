0a9eec3bc446500ee3d2b4f5b70245a68026aeb2
guilherme-antunes2010 <guilherme256578@gmal.com>
2026-05-02 01:07:36 -0300

    modified

--- backend/db.js
+++ backend/db.js
@@ -1,8 +1,10 @@
 const { Pool } = require("pg");
 
 const pool = new Pool({
-  connectionString: "postgresql://batt_resolve_t1_user:5CWEy1RyelUk6gAgI9ACquLTZMxX1iN5@dpg-d7qmvrvlk1mc73cl72j0-a.oregon-postgres.render.com/batt_resolve_t1",
-  ssl: false // local
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false
+  }
 });
 
 module.exports = pool;
